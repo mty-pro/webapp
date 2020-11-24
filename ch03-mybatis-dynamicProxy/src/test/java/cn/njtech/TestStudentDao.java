ac3caa28f1a21c7e399a7cfc1a55c4393e86bf7a
mty-pro <1641284478@qq.com>
2020-11-24 22:42:04 +0800

    通过动态代理方式更便捷地使用mybatis

---
+++ ch03-mybatis-dynamicProxy/src/test/java/cn/njtech/TestStudentDao.java
@@ -0,0 +1,34 @@
+package cn.njtech;
+
+import cn.njtech.dao.StudentDao;
+import cn.njtech.domain.Student;
+import cn.njtech.utils.MyBatisUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.List;
+
+public class TestStudentDao {
+    @Test
+    public void testSelectStudents() {
+        SqlSession sqlSession = MyBatisUtils.getSqlSession();
+//        String sqlId = "cn.njtech.dao.StudentDao.selectStudents";
+//        List<Student> list = sqlSession.selectList(sqlId);
+        StudentDao dao = sqlSession.getMapper(StudentDao.class);
+        List<Student> list = dao.selectStudents();
+        list.forEach(student -> System.out.println(student));
+        sqlSession.close();
+    }
+
+    @Test
+    public void testInsertStudent() {
+        SqlSession sqlSession = MyBatisUtils.getSqlSession();
+        StudentDao dao = sqlSession.getMapper(StudentDao.class);
+        System.out.println("dao=" + dao.getClass().getName());
+        Student student = new Student(19, "wwq", 19);
+        int num = dao.insertStudent(student);
+        sqlSession.commit();
+        System.out.println("添加对象的数量：" + num);
+        sqlSession.close();
+    }
+}
